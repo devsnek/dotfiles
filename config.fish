46bd30be43410a2f74b7ef2878d399afae43b800
Gus Caplan <me@gus.host>
2017-09-09 18:17:48 -0500

    wow

---
+++ config.fish
@@ -0,0 +1,16 @@
+set -gx PATH /Users/gus/bin $PATH
+
+set -g theme_color_scheme solarized-dark
+
+set -gx EDITOR vim
+
+alias https "http --default-scheme=https"
+alias lua "echo lolno"
+alias vi "vim"
+
+alias erc "$EDITOR ~/.config/fish/config.fish"
+alias rrc "source ~/.config/fish/config.fish"
+
+alias uuid 'python -c "import uuid; print str(uuid.uuid4())"'
+
+# test $TERM != "screen" -a $TERM_PROGRAM != "platformio-ide-terminal"; and exec tmux
