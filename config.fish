18a92e7109494935f2e45d9019b54b6111ccf35a
Gus Caplan <me@gus.host>
2017-10-01 19:56:07 -0500

    update aliases

--- config.fish
+++ config.fish
@@ -1,4 +1,7 @@
-set -gx PATH /Users/gus/bin $PATH
+if test -d $HOME/.bin
+  set -gx PATH $HOME/.bin $PATH
+end
+
 set -gx theme_color_scheme solarized-dark
 set -gx EDITOR vim
 
@@ -8,9 +11,51 @@ alias https "http --default-scheme=https"
 alias lua "echo lolno"
 alias vi "vim"
 
+if command --search exa >/dev/null do
+  alias ls="exa"
+end
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
 alias erc "$EDITOR ~/.config/fish/config.fish"
 alias rrc "source ~/.config/fish/config.fish"
 
 alias uuid 'python -c "import uuid; print str(uuid.uuid4())"'
+
+alias timer='echo "Timer started. Stop with Ctrl-D."; and date; and time cat; and date'
+
+alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+
+for method in GET HEAD POST PUT DELETE TRACE OPTIONS
+  alias "$method"="https '$method'"
+end
+
+function fu --description 'Run previous console command with sudo'
+  commandline "sudo $history[1]"
+end
+function fuu -d "Correct your previous console command"
+  set -l fucked_up_command $history[1]
+  env TF_ALIAS=fuu PYTHONIOENCODING=utf-8 thefuck $fucked_up_command | read -l unfucked_command
+  if [ "$unfucked_command" != "" ]
+    eval $unfucked_command
+    history --delete $fucked_up_command
+    history --merge ^ /dev/null
+  end
+end
+alias fuuu=fuu
+alias fuuuu=fuu
+alias fuk=fuu
+alias fuuk=fuu
+alias fuuuk=fuu
+alias fuc=fuu
+alias fuuc=fuu
+alias fuuuc=fuu
+alias fuck=fuu
+alias fuuck=fuu
+alias fuuuck=fuu
 
 # test $TERM != "screen" -a $TERM_PROGRAM != "platformio-ide-terminal"; and exec tmux
