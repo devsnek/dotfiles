c81b76a20de2a273731ad0d7bde1fa21a66a7b4a
Gus Caplan <me@gus.host>
2017-10-21 13:54:36 -0500

    use /Users/gus

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-home=$(realpath ~)
-
 install() { 
   local src="$(pwd)/$1"
-  local dst="${3:-$home}/$1"
+  local dst="${3:-$HOME}/$1"
   echo "linking $src to $dst"
   rm $dst
   ln -s $src $dst
 }
 
-install config.fish false $(realpath ~/.config/fish)
+install config.fish false "$HOME/.config/fish"
 install .tmux.conf
 install .eslintrc.json
 install .vimrc
