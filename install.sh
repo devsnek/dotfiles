23a38221cd224c10defee64407fbfa24a4a934ac
Gus Caplan <me@gus.host>
2017-10-21 14:05:35 -0500

    remove useless arg

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 install() { 
   local src="$(pwd)/$1"
-  local dst="${3:-$HOME}/$1"
+  local dst="${2:-$HOME}/$1"
   echo "linking $src to $dst"
   rm $dst
   ln -s $src $dst
 }
 
-install config.fish false "$HOME/.config/fish"
+install config.fish "$HOME/.config/fish"
 install .tmux.conf
 install .eslintrc.json
 install .vimrc
